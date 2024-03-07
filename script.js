46d96c39862db1f4049e3253c3b52e0d3694df37
islam sobhy <islamsobhyeladly@gmail.com>
2024-03-07 04:04:52 +0200

    Adding The Map Map🗺️📍

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+/*
+-Getting the current postion
+*/
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const currentMap = `https://www.google.com/maps/@=${latitude},${longitude}`;
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 13);
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        'Free Palestine 🇵🇸 ',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function () {
+    alert('CAN NOT GET THE CURRENT LOCATION! 🖐️');
+  }
+);
